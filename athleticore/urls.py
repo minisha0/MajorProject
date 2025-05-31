a0ce00c8c1202739f80a50f7fa8b9a9bc94fb6f9
minisha0 <minishapant@gmail.com>
2025-05-31 15:28:41 +0545

    setup

---
+++ athleticore/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
+    path('events/', include('events.urls')),
+    path('registrations/', include('registrations.urls')),
+    path('announcements/', include('announcements.urls')),
+    path('livegame/', include('livegame.urls')),
+]
